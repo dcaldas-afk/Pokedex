bfb331fd9a88af68dec84b19c32c9048111d1ef1
dcaldas-afk <deciocmleite@gmail.com>
2026-07-22 10:54:30 -0300

    Update backend

--- backend/src/main.py
+++ backend/src/main.py
@@ -11,7 +11,7 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "https://seu-backend.onrender.com/pokemon"
+        "https://pokedex-5oph.onrender.com/pokemon"
         "http://localhost:5173"
     ],
     allow_credentials=True,
